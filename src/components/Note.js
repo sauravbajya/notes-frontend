cc367bc8f421bec2e56eefd53a9909ca2bb2b964
sauravbajya <sauravbajya@gmail.com>
2021-07-20 13:30:36 +0545

    first commit

---
+++ src/components/Note.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important ? 'make not important' : 'make important'
+  return (
+    <li className="note">
+      {note.content}
+      &nbsp;&nbsp;<button onClick={toggleImportance}>{label}</button>
+    </li>
+  )
+}
+export default Note
